4d66995f4ac1076ab4c4df8fca0ad4e67bbdacd1
Chintana Meegamarachchi <chintana@appsolve.com.au>
2016-01-20 09:37:11 +0800

    Initial version

---
+++ Prism6Demo2/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Prism.Unity;
+using Microsoft.Practices.Unity;
+using Prism6Demo2.Views;
+
+namespace Prism6Demo2
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
